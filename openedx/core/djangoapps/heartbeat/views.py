57539ed6e38c5cddc49334f9f768e98470738c46
Joel Barciauskas <joel@edx.org>
2017-04-08 22:21:51 -0400

    A bit of safety around exception handling

--- openedx/core/djangoapps/heartbeat/views.py
+++ openedx/core/djangoapps/heartbeat/views.py
@@ -13,11 +13,16 @@ def heartbeat(request):  # pylint: disable=unused-argument
     of service id: status or message. If the status for any service is anything other than True,
     it returns HTTP code 503 (Service Unavailable); otherwise, it returns 200.
     """
-    check_results = checks(request)
+    check_results = {}
+    try:
+        check_results = checks(request)
 
-    status_code = 200 # Default to OK
-    for check in check_results:
-        if(check_results[check]['status'] == False):
-            status_code = 503 # 503 on any failure
+        status_code = 200 # Default to OK
+        for check in check_results:
+            if(check_results[check]['status'] == False):
+                status_code = 503 # 503 on any failure
+    except e:
+        status_code = 503
+        check_results = {'error': unicode(e)}
 
     return JsonResponse(check_results, status=status_code)
