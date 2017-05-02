1291de2af07de12baeb9eb64ff18bf46aef1ccea
Albert St. Aubin <astaubin@edx.org>
2017-05-02 14:01:14 -0400

    EDUCATOR-141: Moved the Discussions Mgt to its own tab and updated tests

---
+++ lms/static/js/discussions_management/views/discussions_dashboard_factory.js
@@ -0,0 +1,34 @@
+(function(define) {
+    'use strict';
+    define('js/discussions_management/views/discussions_dashboard_factory',
+        ['jquery', 'js/discussions_management/views/discussions',
+            'js/discussions_management/models/divide_discussions',
+            'js/discussions_management/models/course_discussions_settings'],
+        function($, DiscussionsView, DiscussionTopicsSettingsModel, CourseDiscussionsSettingsModel) {
+            return function() {
+                var courseDiscussionSettings = new CourseDiscussionsSettingsModel(),
+                    discussionTopicsSettings = new DiscussionTopicsSettingsModel(),
+                    $discussionsManagementElement = $('.discussions-management'),
+                    discussionsView;
+
+                courseDiscussionSettings.url = $discussionsManagementElement.data('course-discussion-settings-url');
+                discussionTopicsSettings.url = $discussionsManagementElement.data('discussion-topics-url');
+
+                discussionsView = new DiscussionsView({
+                    el: $discussionsManagementElement,
+                    discussionSettings: courseDiscussionSettings,
+                    context: {
+                        discussionTopicsSettingsModel: discussionTopicsSettings,
+                        isCcxEnabled: $discussionsManagementElement.data('is_ccx_enabled')
+                    }
+                });
+
+                courseDiscussionSettings.fetch().done(function() {
+                    discussionTopicsSettings.fetch().done(function() {
+                        discussionsView.render();
+                    });
+                });
+            };
+        });
+}).call(this, define || RequireJS.define);
+
